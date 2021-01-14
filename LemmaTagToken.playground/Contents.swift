2be6ce317dae084592b1720dddac3684823fd8a2
Shailesh Patel <shailesh001@mac.com>
2021-01-14 15:34:45 +0000

    Re-written to use the Natural Language Framework

--- LemmaTagToken.playground/Contents.swift
+++ LemmaTagToken.playground/Contents.swift
@@ -12,7 +12,7 @@ gone before!
 """
 
 extension String {
-    func printlemmas() {
+    func printLemmas() {
         let tagger = NSLinguisticTagger(tagSchemes:[.lemma], options: 0)
         let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
         
@@ -26,6 +26,85 @@ extension String {
             }
         }
     }
+ 
+    func printLemmas2() {
+        // Re-Written as the Natural Language Framework as NSLinguistic Framework is deprecated
+        let tagger = NLTagger(tagSchemes: [.lemma])
+        tagger.string = self
+        
+        let options: NLTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+        tagger.enumerateTags(in: self.startIndex..<self.endIndex, unit: .word, scheme: .lemma, options: options) {
+            (tag, tokenRange) in
+            if let tag = tag {
+                print("\(self[tokenRange]): \(tag.rawValue)")
+            }
+            
+            return true
+        }
+    }
+    
+    func printPartsOfSpeech() {
+        let tagger = NSLinguisticTagger(tagSchemes: [.lexicalClass], options: 0)
+        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+        
+        tagger.string = self
+        let range = NSRange(location: 0, length: self.utf16.count)
+        
+        tagger.enumerateTags(in: range, unit: .word, scheme: .lexicalClass, options: options) {
+            (tag, tokenRange, _) in
+            if let tag = tag {
+                let word = (self as NSString).substring(with: tokenRange)
+                print("\(word) is a \(tag.rawValue)")
+            }
+        }
+    }
+    
+    func printPartsOfSpeech2() {
+        // Re-Written as the Natural Language Framework as NSLinguistic Framework is deprecated
+        let tagger = NLTagger(tagSchemes: [.lexicalClass])
+        tagger.string = self
+        
+        let options: NLTagger.Options = [.omitPunctuation, .omitWhitespace]
+        tagger.enumerateTags(in: self.startIndex..<self.endIndex, unit: .word, scheme: .lexicalClass, options: options) {
+            (tag, tokenRange) in
+            if let tag = tag {
+                print("\(self[tokenRange]): \(tag.rawValue)")
+            }
+            
+            return true
+        }
+    }
+    
+    func printTokens() {
+        let tagger = NSLinguisticTagger(tagSchemes:[.tokenType], options: 0)
+        let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+        
+        tagger.string = self
+        let range = NSRange(location: 0, length: self.utf16.count)
+        
+        tagger.enumerateTags(in: range, unit: .word, scheme: .tokenType, options: options) {
+            (tag, tokenRange, stop) in
+            let word = (self as NSString).substring(with: tokenRange)
+            print(word)
+        }
+    }
+    
+    func printTokens2() {
+        // Re-Written using the Natural Language Framework
+        let tokenizer = NLTokenizer(unit: .word)
+        tokenizer.string = self
+        
+        tokenizer.enumerateTokens(in: self.startIndex..<self.endIndex) {
+            (tokenRange, _) in
+            print("Token: \(self[tokenRange])")
+            return true
+        }
+    }
 }
 
-speech.printlemmas()
+//speech.printLemmas()
+speech.printLemmas2()
+//speech.printPartsOfSpeech()
+speech.printPartsOfSpeech2()
+//speech.printTokens()
+speech.printTokens2()
