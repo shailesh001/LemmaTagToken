b3f6b6278d5f2e6ffd8eccf05fc4a388882ceb18
Shailesh Patel <shailesh001@mac.com>
2021-01-14 14:39:03 +0000

    Initial Commit

---
+++ LemmaTagToken.playground/Contents.swift
@@ -0,0 +1,29 @@
+import NaturalLanguage
+import Foundation
+import CoreML
+
+var str = "Hello, playground"
+
+let speech = """
+Space. the final frontier. These are the voyages of the
+Starship Enterprise.  Its continuing mission to explore strange new worlds,
+to seek out new life and new civilization, to boldly go where no one has
+gone before!
+"""
+
+extension String {
+    func printlemmas() {
+        let tagger = NSLiguisticTagger(tagSchemes:[.lemma], options: 0)
+        let options: NSLiguisticTagger.Options = [.omitPunctuation, .omitWhiteSpace, .joinNames]
+        
+        tagger.string = self
+        let range = NSRange(location: 0, length: self.utf16.count)
+        
+        tagger.enumerateTags(in: range, unit: .word, scheme: .lemma, options: options) {
+            (tag, tokenRange, stop) in
+            if let lemma = tag?.rawValue {
+                print(lemma)
+            }
+        }
+    }
+}
